d1dd9ce5e52dd2426fe4bc6a0a9e9437983ebc62
Apollo Heo <apollobest03@gmail.com>
2018-11-26 05:15:41 +0000

    created 2 semi-done pages: Home and Contacts (still needs to get php file working)

--- master.js
+++ master.js
@@ -1,7 +1,10 @@
-function jokesOnYou() {
-    if (document.getElementById("joke").innerHTML == "Apollo Heo's Engineering Portfolio") {
-       document.getElementById("joke").innerHTML = "You just got hacked!";
-    } else {
-        document.getElementById("joke").innerHTML = "Apollo Heo's Engineering Portfolio";
-    }
+var form_validator = new Validator("contacts_form");
+form_validator.addValidation("name","req","Please provide your name");
+form_validator.addValidation("email","req","Please provide your email");
+form_validator.addValidation("email","email","Please enter a valid email address");
+form_validator.addValidation("website_poll","selone_radio");
+form_validator.addValidation("comments","req");
+
+function validate_form() {
+    return true;
 }
