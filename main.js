a6fd02782502d4efbc271cafbce53a10edf23349
Apollo Heo <apollobest03@gmail.com>
2018-11-29 06:07:17 +0000

    FINISHED CONTACTS PAGE

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ var submitted = false;
 function form_thank_you () {
     submitted = true;
     document.getElementById('gform').style.display = 'none';
-    document.getElementById('thank_you4form').style.display = 'visible';
+    document.getElementById('thank_you4form').style.display = 'initial';
+    document.getElementById('thank_you4form').style.display = 'block';
+    document.getElementById('form_info').style.display = 'none';
 }
