2dbbf552d2612df9b2d9e57f1873f43d8863ead4
Apollo Heo <apollobest03@gmail.com>
2018-11-30 09:39:11 +0000

    ready for the Final Gallery Walk

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
+/* NavBar 
+-- supposed to tell user what each navbar image represents --
+might use data-tooltip in the future;
+might create own boxes to put information;
+might keep current design w/o "tooltip"'s
+*jQuery is needed for the script below
+$("a").hover(function(e) {
+        $($(this).data("tooltip")).css({
+            left: e.pageX + 10,
+            top: e.pageY + 3,
+            display: initial
+        }).stop();
+    }
+    function() {
+        $($(this).data("tooltip")).css({
+            display: hidden;
+        }).stop();
+    });*/
+
 /* Form */
 var submitted = false;
 function form_thank_you () {
     submitted = true;
     document.getElementById('gform').style.display = 'none';
-    document.getElementById('thank_you4form').style.display = 'initial';
     document.getElementById('thank_you4form').style.display = 'block';
     document.getElementById('form_info').style.display = 'none';
 }
